508e593b158d309533bf794cfb40b22bf02a27ab
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-28 08:19:28 -0300

    feat: create route component

---
+++ src/routes/Route.tsx
@@ -0,0 +1,20 @@
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+} from "react-router-dom";
+interface RouteProps extends ReactDOMRouteProps {
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({ component: Component, ...rest }) => {
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={() => {
+        return <Component />;
+      }}
+    />
+  );
+};
+
+export { Route };
