a6a4cfd53492605a2f90a18644cfbcfd9c5e54e2
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-28 08:19:49 -0300

    feat: insert first route to home page

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import { Switch } from "react-router-dom";
+import { Route } from "./Route";
+
+import { Home } from "../pages/Home";
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Home} />
+  </Switch>
+);
+
+export { Routes };
