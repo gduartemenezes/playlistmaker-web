62f85f8f95a8cb9f6e59a269307a0cb3c6a55417
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-28 11:39:57 -0300

    feat: use Playslist card and add method createNewPlaylistHandler

--- src/pages/Home/HomeCard/index.tsx
+++ src/pages/Home/HomeCard/index.tsx
@@ -5,6 +5,7 @@ import { Card } from "../../../components/UI/Card";
 import { Button } from "../../../components/UI/Button";
 import { InfoDiv, Container } from "./styles";
 import { FormControlLabel, RadioGroup, Radio, TextField } from "@mui/material";
+import { PlaylistCard } from "../PlaylistCard/Index";
 
 export function HomeCard(props: any) {
   const [searchType, setSearchType] = useState("cidade");
@@ -12,17 +13,29 @@ export function HomeCard(props: any) {
   const [searchLongitude, setSearchLongitude] = useState("");
   const [searchLatitude, setSearchLatitude] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [showPlaylist, setShowPlaylist] = useState(false);
   function changeSearchHandler(event: SyntheticEvent, newValue: string) {
     console.log(newValue);
     setSearchType(newValue);
   }
+  function createNewPlaylistHandler() {
+    setSearchCidade("");
+    setSearchLatitude("");
+    setSearchLongitude("");
+    setShowPlaylist(false);
+  }
+
+  const playlist = [
+    { id: 0, artist: "Drake", title: "Hotline Bling" },
+    { id: 1, artist: "Drake", title: "Hotline Bling" },
+    { id: 2, artist: "Drake", title: "Hotline Bling" },
+  ];
   const formFields =
     searchType === "cidade" ? (
       <div>
         <TextField
           placeholder="Digite o nome da cidade"
           label="Cidade"
-          defaultValue=""
           variant="filled"
           value={searchCidade}
           onChange={(e) => {
@@ -36,7 +49,6 @@ export function HomeCard(props: any) {
         <TextField
           placeholder="Digite a latitude"
           label="Latitude"
-          defaultValue=""
           variant="filled"
           value={searchLatitude}
           onChange={(e) => {
@@ -47,7 +59,6 @@ export function HomeCard(props: any) {
         <TextField
           placeholder="Digite a longitude"
           label="Longitude"
-          defaultValue=""
           variant="filled"
           value={searchLongitude}
           onChange={(e) => {
@@ -61,13 +72,14 @@ export function HomeCard(props: any) {
     setIsLoading(true);
     setTimeout(() => {
       setIsLoading(false);
+      setShowPlaylist(true);
     }, 3000);
   }
-  return (
+  return !showPlaylist ? (
     <Card className="homeCard">
       <InfoDiv>
         <h3>Vamos começar!</h3>
-        <span>Insira a cidade em que está e descubra uma playlist</span>
+
         <RadioGroup
           aria-labelledby="demo-radio-buttons-group-label"
           defaultValue="cidade"
@@ -89,6 +101,13 @@ export function HomeCard(props: any) {
         </RadioGroup>
       </InfoDiv>
       <Container>
+        <div>
+          <strong>
+            {searchType === "cidade"
+              ? "Insira a cidade em que está e descubra uma playlist"
+              : "Insira as coordernadas e descubra uma playlist"}
+          </strong>
+        </div>
         {formFields}
         <div>
           <Button
@@ -101,5 +120,13 @@ export function HomeCard(props: any) {
         </div>
       </Container>
     </Card>
+  ) : (
+    <PlaylistCard
+      cityName="Goiania"
+      cityTemperature={35}
+      countryName="Brasil"
+      playlist={playlist}
+      onCreatePlaylist={createNewPlaylistHandler}
+    />
   );
 }
